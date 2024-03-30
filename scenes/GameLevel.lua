a51755d1ea163b2bd8d337e6e7b622e456064ca0
Daniel Silvestre <danielsilvestre.work@gmail.com>
2024-03-30 20:38:23 +0100

    Ajout du son du tank sur la carte

--- scenes/GameLevel.lua
+++ scenes/GameLevel.lua
@@ -4,15 +4,10 @@ local Scene   = require "models.scenes.Scene"
 GameLevel     = {}
 
 GameLevel.new = function()
-    order           = order or 0
     local gameLevel = Scene.new("GameLevel", 0)
 
     setmetatable(gameLevel, GameLevel)
     GameLevel.__index = GameLevel
-
-    -- Properties
-
-    -- functions
 
     function gameLevel:update(_)
     end
