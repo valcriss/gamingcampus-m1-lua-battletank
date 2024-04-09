23b0b1ea74bcbe5f3d5dee15471c9b4ea584ebe3
Daniel Silvestre <danielsilvestre.work@gmail.com>
2024-04-09 08:18:03 +0200

    Update ViewPort.lua

--- scenes/models/gameLevel/game/viewport/ViewPort.lua
+++ scenes/models/gameLevel/game/viewport/ViewPort.lua
@@ -1,25 +1,25 @@
 local Component = require "models.scenes.Component"
 local Rectangle = require "models.drawing.Rectangle"
-local Vector2   = require "models.drawing.Vector2"
+local Vector2 = require "models.drawing.Vector2"
 
 ---@class ViewPort
-ViewPort        = {}
+ViewPort = {}
 
 ---@param gameManager GameManager
-ViewPort.new    = function(gameManager)
+ViewPort.new = function(gameManager)
 
     local viewPort = Component.new("ViewPort", {
         gameManager = gameManager
     }, 0, 0)
 
     setmetatable(viewPort, ViewPort)
-    ViewPort.__index   = ViewPort
+    ViewPort.__index = ViewPort
 
     -- ---------------------------------------------
     -- Properties
     -- ---------------------------------------------
-    local maxBounds    = Rectangle.new()
-    local realBounds   = Rectangle.new()
+    local maxBounds = Rectangle.new()
+    local realBounds = Rectangle.new()
     local renderBounds = Rectangle.new()
 
     -- ---------------------------------------------
@@ -28,7 +28,7 @@ ViewPort.new    = function(gameManager)
     ---@public
     function viewPort.load()
         maxBounds = Rectangle.new(0, 0, gameManager.getGameLevelData().data.level.Width * gameManager.getGameLevelData().data.level.TileSize, gameManager.getGameLevelData().data.level.Height * gameManager.getGameLevelData().data.level.TileSize)
-        viewPort.bounds.setPoint(gameManager.getGameLevelData().data.level.StartX * gameManager.getGameLevelData().data.level.TileSize, gameManager.getGameLevelData().data.level.StartY * gameManager.getGameLevelData().data.level.TileSize)
+        viewPort.bounds.setPoint((gameManager.getGameLevelData().data.level.StartX - 1) * gameManager.getGameLevelData().data.level.TileSize, (gameManager.getGameLevelData().data.level.StartY - 1) * gameManager.getGameLevelData().data.level.TileSize)
     end
 
     ---@public
@@ -72,7 +72,7 @@ ViewPort.new    = function(gameManager)
 
     function viewPort.playerInputs(dt, top, left, bottom, right)
         local previousBounds = Rectangle.new(viewPort.bounds.x, viewPort.bounds.y, viewPort.bounds.width, viewPort.bounds.height)
-        local speed          = 500
+        local speed = 500
 
         if top then
             viewPort.verticalMove(-speed * dt)
@@ -93,8 +93,8 @@ ViewPort.new    = function(gameManager)
     end
 
     function viewPort.newPositionIsInvalid()
-        local ref           = viewPort.bounds.getPoint()
-        local border        = (gameManager.getGameLevelData().data.level.TileSize)
+        local ref = viewPort.bounds.getPoint()
+        local border = (gameManager.getGameLevelData().data.level.TileSize)
         local tilePosition1 = gameManager.getGameLevelData().getGridPosition(ref.x, ref.y)
         local tilePosition2 = gameManager.getGameLevelData().getGridPosition(ref.x + border, ref.y)
         local tilePosition3 = gameManager.getGameLevelData().getGridPosition(ref.x + border, ref.y + border)
@@ -136,8 +136,8 @@ ViewPort.new    = function(gameManager)
 
     ---@private
     function viewPort.updateRenderBounds()
-        local drawX  = (screenManager:getWindowWidth() / 2) - viewPort.bounds.x - (gameManager.getGameLevelData().data.level.TileSize / 2)
-        local drawY  = (screenManager:getWindowHeight() / 2) - viewPort.bounds.y - (gameManager.getGameLevelData().data.level.TileSize / 2)
+        local drawX = (screenManager:getWindowWidth() / 2) - viewPort.bounds.x - (gameManager.getGameLevelData().data.level.TileSize / 2)
+        local drawY = (screenManager:getWindowHeight() / 2) - viewPort.bounds.y - (gameManager.getGameLevelData().data.level.TileSize / 2)
         renderBounds = Rectangle.new(drawX, drawY, screenManager:getWindowWidth(), screenManager:getWindowHeight())
     end
 
