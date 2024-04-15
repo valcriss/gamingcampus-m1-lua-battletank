64b6069de2e6356be2d923c1dc9597b56035f367
Daniel Silvestre <danielsilvestre.work@gmail.com>
2024-04-15 15:24:46 +0200

    Refactoring des components

--- models/ui/Button.lua
+++ models/ui/Button.lua
@@ -5,31 +5,33 @@ local SoundEffect = require "models.audio.SoundEffect"
 ---@class Button
 Button            = {}
 
+---@param name string
+---@param assetPath string
+---@param assetOverPath string
+---@param assetPressedPath string
+---@param x number
+---@param y number
+---@param text string
+---@param action function
 Button.new        = function(name, assetPath, assetOverPath, assetPressedPath, x, y, text, action)
-    local button = Component.new(
-            name,
-            {
-                assetPath        = assetPath,
-                assetOverPath    = assetOverPath,
-                assetPressedPath = assetPressedPath,
-                text             = text,
-                action           = action,
-                mouseIsOver      = false,
-                mouseIsPressed   = false
-            },
-            x,
-            y,
-            190,
-            49
-    )
+    local button = Component.new(name, x, y, 190, 49)
 
     setmetatable(button, Button)
     Button.__index           = Button
 
-    local buttonText         = BitmapText.new(button.name .. "_buttonText", "assets/ui/ui-18.fnt", button.data.text, "center", "center", button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
-    local buttonImage        = Image.new(button.name .. "_buttonImage", button.data.assetPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
-    local buttonOverImage    = Image.new(button.name .. "_buttonOverImage", button.data.assetOverPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
-    local buttonPressedImage = Image.new(button.name .. "_buttonPressedImage", button.data.assetPressedPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
+    -- ---------------------------------------------
+    -- Properties
+    -- ---------------------------------------------
+    local mouseIsOver        = false
+    local mouseIsPressed     = false
+
+    -- ---------------------------------------------
+    -- Components
+    -- ---------------------------------------------
+    local buttonText         = BitmapText.new(button.name .. "_buttonText", "assets/ui/ui-18.fnt", text, "center", "center", button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
+    local buttonImage        = Image.new(button.name .. "_buttonImage", assetPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
+    local buttonOverImage    = Image.new(button.name .. "_buttonOverImage", assetOverPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
+    local buttonPressedImage = Image.new(button.name .. "_buttonPressedImage", assetPressedPath, button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
     local soundEffect        = SoundEffect.new(button.name .. "_soundEffect", "assets/ui/switch2.ogg", "stream", false, false, configuration:getSoundVolume())
 
     button.addComponent(buttonImage)
@@ -38,6 +40,11 @@ Button.new        = function(name, assetPath, assetOverPath, assetPressedPath, x
     button.addComponent(buttonText)
     button.addComponent(soundEffect)
 
+    -- ---------------------------------------------
+    -- Public functions
+    -- ---------------------------------------------
+
+    ---@public
     function button.update(_)
         soundEffect.setVolume(configuration:getSoundVolume())
         buttonImage.setPosition(button.bounds.x + (button.bounds.width / 2), button.bounds.y + (button.bounds.height / 2))
@@ -47,29 +54,29 @@ Button.new        = function(name, assetPath, assetOverPath, assetPressedPath, x
 
         local mouseX, mouseY = love.mouse.getPosition()
         if (mouseX == nil or mouseY == nil) then return end
-        local isDown               = love.mouse.isDown(1)
-        local wasPressed           = not isDown and button.data.mouseIsPressed
-        button.data.mouseIsPressed = isDown
-        button.data.mouseIsOver    = button.bounds.containsPoint(screenManager:ScaleUIValueX(mouseX), screenManager:ScaleUIValueY(mouseY))
-        if not button.data.mouseIsOver then button.data.mouseIsPressed = false end
-        if not button.data.mouseIsOver then
+        local isDown     = love.mouse.isDown(1)
+        local wasPressed = not isDown and mouseIsPressed
+        mouseIsPressed   = isDown
+        mouseIsOver      = button.bounds.containsPoint(screenManager:ScaleUIValueX(mouseX), screenManager:ScaleUIValueY(mouseY))
+        if not mouseIsOver then mouseIsPressed = false end
+        if not mouseIsOver then
             buttonImage.show()
             buttonOverImage.hide()
             buttonPressedImage.hide()
-        elseif button.data.mouseIsOver and not isDown then
+        elseif mouseIsOver and not isDown then
             buttonImage.hide()
             buttonOverImage.show()
             buttonPressedImage.hide()
-        elseif button.data.mouseIsOver and isDown then
+        elseif mouseIsOver and isDown then
             buttonImage.hide()
             buttonOverImage.hide()
             buttonPressedImage.show()
         end
 
-        if (button.data.action == nil) then return end
-        if button.data.mouseIsOver and wasPressed then
+        if (action == nil) then return end
+        if mouseIsOver and wasPressed then
             soundEffect.play()
-            button.data.action()
+            action()
         end
     end
 
